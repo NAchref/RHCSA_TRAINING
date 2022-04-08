c5804b10d7d28db645a7b7b3a562ddb91faf8606
NAchref <64566472+NAchref@users.noreply.github.com>
2022-04-08 12:43:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,12 +7,12 @@ Configure a HTTP server, which can be accessed throught http://station.domaine40
 
 Correct Answer
 
-#yum install -y httpd
-# chkconfig httpd on
-# cd /var/www/html
-# wget http://ip/dir/example.html
-# cp example.com index.html
-# vim /etc/httpd/conf/httpd
+yum install -y httpd
+chkconfig httpd on
+cd /var/www/html
+wget http://ip/dir/example.html
+cp example.com index.html
+vim /etc/httpd/conf/httpd
 
 
 Question :
