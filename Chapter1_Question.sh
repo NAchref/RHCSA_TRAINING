9a79e7df9f2be72feff16187333d26f13b41f911
NAchref <64566472+NAchref@users.noreply.github.com>
2022-04-12 12:20:03 +0100

    Create Chapter1_Question.sh

---
+++ Chapter1_Question.sh
@@ -0,0 +1,30 @@
+1. You want to install a test environment to practice for the RHCSA exam.
+Which of the following distributions should you avoid?
+
+a. The most recent Fedora version
+b. CentOS 8
+c. Scientific Linux 8
+d. RHEL 8
+
+
+2. Which of the following features is available in both RHEL and CentOS?
+
+a. Hardware certification
+b. Software certification
+c. The right to make support calls
+d. Software updates
+
+3. Why should you install the server with a GUI installation pattern?
+
+a. To prepare for RHCSA, you need some tools that run in a GUI only.
+b. The minimal installation is incomplete.
+c. If you do not install a GUI immediately, it is hard to add it later.
+d. The Server with GUI is the default installation that is recommended by
+Red Hat.
+
+
+4. Which is the default file system that is used in RHEL 8?
+a. Ext3
+b. Ext4
+c. XFS
+d. Btrfs
