c36866120e304281f5d8d479bbb66c2a0992e2f2
NAchref <64566472+NAchref@users.noreply.github.com>
2022-04-17 17:38:26 +0100

    Create chapiter 1.sh

---
+++ chapiter 1.sh
@@ -0,0 +1,14 @@
+
+Linux distribution closely related to Red Hat Enterprise Linux is Fedora, 
+a completely open source Linux distribution that is available for free.
+
+Red Hat has a lot of staff dedicated to contributing to the Fedora project, because Red Hat uses 
+Fedora as the development platform for RHEL.
+
+Important ! :
+which is why you should 
+not use Fedora to prepare for the RCHSA exam
+
+Fedora is also used by Red Hat as a testing ground for new features that might or might not be included in future 
+RHEL releases. If you were to choose Fedora, you would be working with items 
+that are not available in RHEL, which means that you would have to do things differently on the exam. So, don’t use it
